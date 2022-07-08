f75738f34ea37517c10891e15ceb16d19e6c70c6
amy <ctrlalternativedelete@gmail.com>
2022-07-09 05:31:27 +1000

    fixed up before each

--- layouts/default.vue
+++ layouts/default.vue
@@ -85,6 +85,7 @@ export default {
   async mounted() {
     this.$router.beforeEach((_to, _from) => {
       this.$accessor.SET_LOADING(false);
+      return true;
     });
 
     this.$accessor.SET_USER(await this.$supabase.auth.user());
