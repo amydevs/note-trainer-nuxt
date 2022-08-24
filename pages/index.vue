5dd07aa34b26f4405a8c45886237a3ea192222aa
Amy Y <ctrlalternativedelete@gmail.com>
2022-08-25 06:15:58 +1000

    replaced icon

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,10 @@
   <v-container>
     <v-row justify="center" align="center">
       <v-col cols="12" sm="8" md="6">
-        <!-- <v-card class="logo py-4 d-flex justify-center">
+        <v-card class="logo py-4 d-flex justify-center">
           <NuxtLogo />
-          <VuetifyLogo />
-        </v-card> -->
+          <!-- <VuetifyLogo /> -->
+        </v-card>
         <v-card>
           <v-card-title class="headline">
             Welcome to Note-Trainer
