4505099a6500810a56db9337d1ed092ae195fe09
Amy Y <ctrlalternativedelete@gmail.com>
2022-08-24 19:31:09 +1000

    changed intro

--- pages/index.vue
+++ pages/index.vue
@@ -8,68 +8,19 @@
         </v-card>
         <v-card>
           <v-card-title class="headline">
-            Welcome to the Vuetify + Nuxt.js template
+            Welcome to Note-Trainer
           </v-card-title>
           <v-card-text>
-            <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-            <p>
-              For more information on Vuetify, check out the <a
-                href="https://vuetifyjs.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                documentation
-              </a>.
-            </p>
-            <p>
-              If you have questions, please join the official <a
-                href="https://chat.vuetifyjs.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="chat"
-              >
-                discord
-              </a>.
-            </p>
-            <p>
-              Find a bug? Report it on the github <a
-                href="https://github.com/vuetifyjs/vuetify/issues"
-                target="_blank"
-                rel="noopener noreferrer"
-                title="contribute"
-              >
-                issue board
-              </a>.
-            </p>
-            <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-            <div class="text-xs-right">
-              <em><small>&mdash; John Leider</small></em>
-            </div>
-            <hr class="my-3">
-            <a
-              href="https://nuxtjs.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Nuxt Documentation
-            </a>
-            <br>
-            <a
-              href="https://github.com/nuxt/nuxt.js"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Nuxt GitHub
-            </a>
+            Note-Trainer is a federated and free platform for music education. It provides activities and lessons to both high-school and independent students at no cost. For educators, it provides a comprehensive system for music theory lesson plans and allows them to better manage and supervise the learning of their music students.
           </v-card-text>
           <v-card-actions>
             <v-spacer />
             <v-btn
               color="primary"
               nuxt
-              to="/inspire"
+              to="/play"
             >
-              Continue
+              Play
             </v-btn>
           </v-card-actions>
         </v-card>
