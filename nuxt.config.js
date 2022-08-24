95fde3757a60d379ccae4926d29a3e503d9dd24d
Amy Y <ctrlalternativedelete@gmail.com>
2022-08-25 04:24:17 +1000

    edit title

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,8 +8,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - note-trainer-nuxt',
-    title: 'note-trainer-nuxt',
+    titleTemplate: '%s',
+    title: 'Note-Trainer',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
